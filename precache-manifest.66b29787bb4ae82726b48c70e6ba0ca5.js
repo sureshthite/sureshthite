b0517e71b0632c076d9f34d73d6cb90f8d059ccc
sureshthite <sureshsthite@gmail.com>
2019-09-14 22:03:56 +0530

    Updates

---
+++ precache-manifest.66b29787bb4ae82726b48c70e6ba0ca5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e189a5349fc28b9a9c67",
+    "url": "/sureshthite/static/js/runtime~main.e189a534.js"
+  },
+  {
+    "revision": "177afca93d8b3f5120fe",
+    "url": "/sureshthite/static/js/main.177afca9.chunk.js"
+  },
+  {
+    "revision": "d49a7807bdd707278c9a",
+    "url": "/sureshthite/static/js/2.d49a7807.chunk.js"
+  },
+  {
+    "revision": "177afca93d8b3f5120fe",
+    "url": "/sureshthite/static/css/main.88869bbf.chunk.css"
+  },
+  {
+    "revision": "c1804f744b9597eb1d5d2e3517afe007",
+    "url": "/sureshthite/index.html"
+  }
+];
